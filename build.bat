1ca5be5925889ad69ab31a14157905cb1e7f1075
Krzem <krzem5.dev@gmail.com>
2021-04-17 14:00:16 +0200

    Push Update 04/17/2021, 14:00:08

--- build.bat
+++ build.bat
@@ -1,5 +1,3 @@
 @echo off
-echo NUL>_.class&&del /s /f /q *.class
 cls
-javac -encoding utf8 -cp com/krzem/fischertechnic_disassembler/modules/jna-5.5.0.jar;com/krzem/fischertechnic_disassembler/modules/jna-platform-5.5.0.jar; com/krzem/fischertechnic_disassembler/Main.java&&java -Dfile.encoding=UTF8 -cp com/krzem/fischertechnic_disassembler/modules/jna-5.5.0.jar;com/krzem/fischertechnic_disassembler/modules/jna-platform-5.5.0.jar; com/krzem/fischertechnic_disassembler/Main temp.rpp
-start /min cmd /c "echo NUL>_.class&&del /s /f /q *.class"
+python build.py %*
